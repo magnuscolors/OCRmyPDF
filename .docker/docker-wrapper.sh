54cb3d3f29eed701c0507b81111205b2f0efd0cf
Willem Hulshof <w.hulshof@magnus.nl>
2020-01-16 23:34:02 +0100

    docker-wrapper.sh

---
+++ .docker/docker-wrapper.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# 1. Watch WATCHED_DIR folder recursively.
+# 2. in ocr_and_email_92.sh: OCR pdf-images to multipage pdf documents with tex$
+#
+#change the variable below with the folder you want this script to watch for ne$
+
+. /appenv/bin/activate
+cd /home/docker
+
+WATCHED_DIR=/mnt/omp/pdf_in
+export SHELL=/bin/bash
+(echo start; inotifywait -mr -e create,moved_to "$WATCHED_DIR" --format "%w%f") |
+while read event; do
+
+                 oIFS=$IFS
+                 IFS=$'\n'
+                 find $WATCHED_DIR -iname *.pdf |
+                 while read -r file; do
+                        fileocr="${file/pdf_in/pdf_ocr}"
+                        filedecrypt="${file/pdf_in/pdf_dec}"
+                        filedecerror="${file/pdf_in/pdf_dec_error}"
+                        fileorig="${file/pdf_in/pdf_orig}"
+                        filename="`basename $file`"
+                        shopt -s nocasematch
+                        case "$filename" in
+                        .*.pdf) rm "$file" && echo "$file" "deleted";;
+                        *.pdf) ocrmypdf --deskew --output-type pdf --skip-text -l "eng+nld" "$file" "$fileocr"
+                        return_code=$?
+                        if [ $return_code -eq 0 ]
+                            then
+                              echo "$file" "ocr-ed"
+                        else
+                            if [ $return_code -eq 8 ]
+                                then
+                                    echo "$file" "encrypted, decrypting" && \
+                                    qpdf --decrypt "$file" "$filedecrypt"
+                                if [ $? -eq 0 ]
+                                    then
+                                        echo "$file" "decrypted"
+                                else
+                                        echo "$file" "not decrypted" && \
+                                        rm "$filedecrypt" && \
+                                        echo "$file" "deleted from pdf_dec" && \
+                                        cp "$file" "$filedecerror" && \
+                                        cp "$file" "$fileocr" && \
+                                        mv "$file" "$fileorig" && \
+                                        continue
+                                fi
+                                ocrmypdf --deskew --output-type pdf --skip-text -l "eng+nld" "$filedecrypt" "$fileocr" && \
+                                    echo "$file" "ocr-ed" && \
+                                rm "$filedecrypt" && \
+                                    echo "$file" "deleted from pdf_dec"
+                            fi
+                        fi
+                               mv "$file" "$fileorig" && \
+                                    echo "$file" "moved to orig" ;;
+                        *) mv "$file" "$fileorig" && echo "$file" "Oops" ;;
+                        esac
+                        shopt -u nocasematch
+                 done
+                 IFS=$oIFS
+done
